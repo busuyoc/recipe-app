ea3768cb0d612403436919bf64ced9b5e62a8580
busuyoc <busuioc.p.claudiu@gmail.com>
2025-05-22 15:32:56 +0300

    reinit api

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,29 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8906'
+    );
+    const data = await res.json();
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(data);
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      imageUrl: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.log(err);
+  }
+};
+showRecipe();
